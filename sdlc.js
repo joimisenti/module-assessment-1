c1eb00d9e905e35b5a32e25c28c5b2be4d5bbe79
Joi Misenti <joipolloi1@gmail.com>
2023-06-24 20:43:19 -0400

    finished software dev life cycle

--- sdlc.js
+++ sdlc.js
@@ -5,12 +5,12 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
-// Implementation
-// Design
-// Maintain
-// Analysis of Requirements
-// Testing & Integration
 // Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
@@ -18,4 +18,9 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+const analysisofRequirements = "Business analysts determine resources and costs of a project. Developers identify their tech stack. What software, system, technologies, and materials will be chosen and required before beginning to build the code. Requirements are analyzed from both the technical side of the developer's as well as the customer side of the client's; for instance, what does the client want?"
+const design = "The major aspects of the product are drawn up and outlined, including the architecture, features, and security measures. This is the consideration for the appearance of the software, where UI and UX developers design the interactivity and interface, such as if there will be inputs."
+const implementation = "The ideas and features are actively coded into script. Coders will take the design blueprints and develop the code in the necessary language(s), making the features and structures."
+const testingandIntegration = "The code is evaluated and assessed for functionality and performance through testing. Is the code working as intended? Is the product stable? Quality assessment is carried out as bugs are uncovered and addressed. Integration refers to making the code available online and accessible to others for use."
+const maintain = "After deployment, the software is continuously monitored, updated, and patched. Based on feedback and reports, the software undergoes version upgrades and/or addition of new features."
